eb7c4ef1cf59a901d0e712829a26e684e9c222b7
Dominik Nitschmann <dominik.nitschmann@googlemail.com>
2019-11-26 10:31:37 +0100

    Fix reference error

--- frontend/src/app/filter/filter.component.ts
+++ frontend/src/app/filter/filter.component.ts
@@ -12,7 +12,7 @@ export class FilterComponent implements OnInit {
   supported_number_of_travellers = [1,2,3,4,5]
 
   // Default values
-  model = new Request("Amsterdam", "Rotterdam", supported_number_of_travellers[0], 5, 5, 5, 6.5);
+  model = new Request("Amsterdam", "Rotterdam", this.supported_number_of_travellers[0], 5, 5, 5, 6.5);
 
   constructor() { }
 
